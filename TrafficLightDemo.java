6b2f1916db40d84b0059b13fef446122842f7be2
Cameron Imanpour <34729011+CIMAN01@users.noreply.github.com>
2022-05-16 16:33:25 -0700

    Clean up code

--- TrafficLightDemo.java
+++ TrafficLightDemo.java
@@ -1,38 +1,33 @@
-// A simulation of a traffic light that uses an enumeration to describe the light's color.
 
-// main class
+// A simulation of a traffic light that uses an enumeration to describe the light's color.
 class TrafficLightDemo {
 
-    // main method
     public static void main(String[] args) {
-        // create a new traffic light object and pass in the color green
+        // create a new traffic light object that uses the color green
         TrafficLightSimulator trafficLight = new TrafficLightSimulator(TrafficLightColor.GREEN);
-        // create a new thread and pass in the traffic light object
+        // create a new thread that uses the traffic light object
         Thread thread = new Thread(trafficLight);
-        // run the threat
+        // run the thread
         thread.start();
-        // use a for-loop to log from 0 to less than 9
+        // iterate from 1 to 9 (9 colors)
         for (int i = 0; i < 9; i++) {
-            // display the current traffic light color
-            System.out.println(trafficLight.getColor());
+            System.out.println(trafficLight.getColor()); // print the current traffic light color
             // wait for the traffic light to change
             trafficLight.waitForChange();
         }
-        // cancel the traffic light simulation
+        // cancel the traffic light simulation via a method
         trafficLight.cancel();
     }
 
-    /*
-
-    GREEN
-    YELLOW
-    RED
-    GREEN
-    YELLOW
-    RED
-    GREEN
-    YELLOW
-    RED
-
+    /* OUTPUT:
+            GREEN
+            YELLOW
+            RED
+            GREEN
+            YELLOW
+            RED
+            GREEN
+            YELLOW
+            RED
     */
 }
