f8274b718d077d550e87c0d7435dc10e8a89c492
Cameron Imanpour <34729011+CIMAN01@users.noreply.github.com>
2022-05-24 16:03:11 -0700

    Reformat code

--- TrafficLightColor.java
+++ TrafficLightColor.java
@@ -1,11 +1,15 @@
 
 // an enumeration of the colors of a traffic light (whenever the color of the light is needed, its enumeration value is used)
 enum TrafficLightColor {
+    
     RED(12000), GREEN(10000), YELLOW(2000);
+    
     private final int delay; // duration of sleep
+    
     TrafficLightColor(int delay) {
         this.delay = delay;
     }
+    
     public int getDelay() {
         return delay;
     }
