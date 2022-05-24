d977a0513962efb5b6d8026181a2d6295ca58ee3
Cameron Imanpour <34729011+CIMAN01@users.noreply.github.com>
2022-05-24 16:05:04 -0700

    Refactor code and improve comments

--- TrafficLightSimulator.java
+++ TrafficLightSimulator.java
@@ -1,35 +1,41 @@
-
 /*** a computerized traffic light - a class that encapsulates the traffic light simulation ***/
 
 // implementing Runnable is necessary because a separate thread is used to run each traffic light (this thread will cycle through the colors)
 class TrafficLightSimulator implements Runnable {
+    
     private TrafficLightColor tlc; // holds the traffic light color
+    
     boolean stop = false; // set to true to stop the simulation
     boolean changed = false; // true when the light has changed
+    
+    private final Object tlcLock = new Object(); // use a dedicated monitor object
+    private final Object stopLock = new Object(); // use a dedicated monitor object
+
     TrafficLightSimulator(TrafficLightColor col) {
         tlc = col;
     }
 
-    // a method that begins running the traffic light
+    // This method cycles the light through the different colors
     @Override
-    public void run() { // start up the light
+    public void run() {
         // while we haven't stopped running the traffic light simulation
         while(!stop) {
-            // handle potential exceptions
+            // handle any exceptions
             try {
-                // cycles the light through the colors
-                Thread.sleep(tlc.getDelay()); // use ordinal values instead of relying on a switch-statement
+                // sleeps an appropriate amount of time, based on the current color
+                Thread.sleep(tlc.getDelay()); // using ordinal values instead of relying on a switch-statement
             }
             catch(InterruptedException e) {
-                System.out.println(e);
+                e.printStackTrace();
             }
-            changeColor(); // calls changeColor( ) to change to the next color in the sequence
+            // change to the next color in the sequence
+            changeColor();
         }
     }
 
-    // a method that changes color
-    synchronized void changeColor() {
-        // assigns the next color in the sequence using a switch-statement
+    // This method cycles to the next color in the sequence
+    public synchronized void changeColor() {
+        // use a switch-statement to assign the next color in the sequence
         switch(tlc) {
             case RED:
                 tlc = TrafficLightColor.GREEN;
@@ -39,37 +45,42 @@ class TrafficLightSimulator implements Runnable {
                 break;
             case GREEN:
                 tlc = TrafficLightColor.YELLOW;
+                break;
         }
-
-        changed = true; // true if light has changed
-
+        // set changed to true when a light has changed
+        changed = true;
         // notify() method is synchronized and can therefor be called only from a synchronized context
-        notify(); // signal that the light has changed
+        notify(); // signal that the light has changed (wakes up a single thread that is waiting on that object's monitor)
     }
 
-    // a method that waits for the light to change - this method simply calls wait() and won’t return until the color has changed
-    synchronized void waitForChange() {  // wait until a light change occurs
+    // This method waits until the color of the light is changed (this method simply calls wait() and won’t return until the color has changed)
+    public synchronized void waitForChange() {
         while(!changed) {
-            // handle potential exceptions
+            // handle any exceptions
             try {
                 // this call won’t return until changeColor() executes a call to notify()
-                wait(); // wait for light to change
+                wait(); // wait for light to change (causes the current thread to wait until another thread invokes the notify() method)
             }
             catch(InterruptedException e) {
-                System.out.println(e);
+                e.printStackTrace();
             }
         }
         changed = false; // reset
     }
 
-    synchronized TrafficLightColor getColor() {
-        // return the current color
-        return tlc;
+    // return the current color
+    public TrafficLightColor getColor() {
+        synchronized(tlcLock) {
+            // return the current color
+            return tlc;
+        }
     }
 
-    // stops the traffic light thread
-    synchronized void cancel() {
-        stop = true;
+    // stop the traffic light
+    public void cancel() {
+        synchronized(stopLock) {
+            stop = true;
+        }
     }
 
 }
